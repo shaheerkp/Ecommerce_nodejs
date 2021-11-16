0e1d63a709244fdffddf7f1992d7a34cd586b68b
shaheerkp <kpshaheer123@gmail.com>
2021-11-17 03:00:20 +0530

    final fix paypal url

--- routes/user.js
+++ routes/user.js
@@ -109,10 +109,9 @@ router.post('/pay', async (req, res) => {
         "payment_method": "paypal"
       },
       "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
-        // "return_url": "https://ecom.shaheerkp.tech/success",
-        // "cancel_url": "https://ecom.shaheerkp.tech/cancel"
+       
+        "return_url": "https://ecom.shaheerkp.tech/success",
+        "cancel_url": "https://ecom.shaheerkp.tech/cancel"
       },
       "transactions": [{
         "item_list": {
